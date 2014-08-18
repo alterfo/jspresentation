b4720d77833e9c34e259e42668e4d3b6eda5df77
Олег Сидоркин <olegsidorkin@NameOfComputer.local>
2014-08-18 07:57:30 +0400

    added smth

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,54 +1,50 @@
-// Generated on 2014-07-22 using generator-revealjs 0.0.2
-'use strict';
-var LIVERELOAD_PORT = 8080;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({
+    port: LIVERELOAD_PORT
+});
+
+var livereloadMiddleware = function(connect, options) {
+    return [
+        lrSnippet,
+        connect.static(options.base),
+        connect.directory(options.base)
+    ];
 };
- 
-module.exports = function (grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
- 
-  grunt.initConfig({
-    watch: {
-      options: {
-        nospawn: true,
-        livereload: LIVERELOAD_PORT
-      },
-      livereload: {
-        files: [
-          'slides/{,*/}*.html',
-		  'slides/{,*/}*.js',
-          'slides/css/{,*/}*.css',
-		  'slides/*.md'
-        ]
-      }
-    },
-    connect: {
-      options: {
-        port: 80,
-        base:'slides',
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: '0.0.0.0'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, '.')
-            ];
-          }
+
+
+module.exports = function(grunt) {
+
+    // Load Grunt tasks declared in the package.json file
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // Configure Grunt 
+    grunt.initConfig({
+
+        connect: {
+            client: {
+                options: {
+                    port: 9000,
+                    base: 'slides',
+                    middleware: livereloadMiddleware
+                }
+            }
+        },
+
+
+
+        watch: {
+            client: {
+
+                files: ['slides/**/*'],
+
+                tasks: [],
+                options: {
+                    livereload: LIVERELOAD_PORT
+                }
+            }
         }
-      }
-    },
-    open: {
-      server: {
-        path: 'http://localhost:<%= connect.options.port %>/slides/'
-      }
-    }
-  });
- 
-  grunt.registerTask('default', ['connect:livereload', 'open', 'watch']);
+    });
+
+    grunt.registerTask('default', ['connect:client', 'watch:client']);
+
 };
